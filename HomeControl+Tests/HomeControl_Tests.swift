5296312b2d085dff9b91007a00a8fa19b09d1efd
BoGlavatki <glavatkiboleslav@gmail.com>
2024-09-29 18:18:53 +0200

    Initial Commit

---
+++ HomeControl+Tests/HomeControl_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  HomeControl_Tests.swift
+//  HomeControl+Tests
+//
+//  Created by Boleslav Glavatki on 29.09.24.
+//
+
+import Testing
+@testable import HomeControl_
+
+struct HomeControl_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
